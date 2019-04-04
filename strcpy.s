d55e56ca479d52f6c278755ff162197a69eab37b
3670460 <paul-antoine.levesque@etu.upmc.fr>
2019-04-04 20:42:25 +0200

    update

--- strcpy.s
+++ strcpy.s
@@ -2,4 +2,17 @@
 
 strcpy : #$4 = trg $5 = src
 
-  
+  ori $2 $4 0 #on met $4 dans l'adresse de retour
+
+while :
+
+  lb $6 0($5) #on met dans $6 la valeur de src au byte 0
+  beq $6 $0 end_while
+  sb $4 $6
+  addi $4 $4 1
+  addi $5 $5 1
+  j while
+
+end_while :
+
+  jr $31
